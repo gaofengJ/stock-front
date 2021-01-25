4fe31dd44c13ebf78682f478952504991f354ba1
gaofeng_jin <gaofeng_jin@intsig.net>
2021-01-25 17:37:53 +0800

    [RC]首页添加公众号图片

--- src/views/home/index/index.vue
+++ src/views/home/index/index.vue
@@ -2,7 +2,8 @@
   <div class="tips">
     此网站纯属个人兴趣，目前可用功能：复盘数据（涨跌统计、短线情绪）。
     后续会陆续更新功能。<br><br>
-    欢迎关注个人公众号：木风同学
+    欢迎关注个人公众号：木风同学<br>
+    <img src="http://www.mufengtongxue.com/images/img_qrcode.jpg">
   </div>
 </template>
 <script>
